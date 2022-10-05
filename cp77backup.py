a533bc562facd2d6280aeebf58307689aaf85386
S. Hall <shall.usnavy@gmail.com>
2022-10-05 10:39:23 -0400

    refactor: Reorganize source a little
    This is more of an intended starting point to build off of

--- cp77backup.py
+++ cp77backup.py
@@ -5,11 +5,11 @@ import os
 from tkinter import *
 
 
-def backup() -> str:
+def backup() -> None:
     save_path = Path.home().joinpath('Saved Games', 'CD Projekt Red')
     source = os.path.join(save_path, "Cyberpunk 2077")
-    if not os.path.exists(source):
-        return "Cyberpunk saved games folder cannot be located. Exiting."
+
+    lbl_result["text"] = check_source(source)
 
     today = date.today()
     dst_format = "Cyberpunk 2077" + today.strftime("__%d_%b_%Y")
@@ -18,7 +18,13 @@ def backup() -> str:
     target = shutil.make_archive(destination, "zip", source)
     target_size = get_file_size(target)
 
-    return f"Save folder size: {source_size}, compressed backup size: {target_size}. Done!"
+    lbl_result["text"] = f"Save folder size: {source_size}, compressed backup size: {target_size}. Done!"
+
+
+def check_source(source) -> str:
+    if not os.path.exists(source):
+        return "Cyberpunk saved games folder cannot be located. Exiting."
+    return "Save folder found."
 
 
 def get_dir_size(target_dir) -> str:
@@ -33,24 +39,20 @@ def get_file_size(target_file) -> str:
     return f"{raw_size:.1f}M"
 
 
-# backup()
-# Tk GUI
 window = Tk()
 window.title("Cyberpunk 2077 Savegame Backup")
 window.columnconfigure(1, minsize=450)
 window.rowconfigure(5, minsize=150)
 
 btn_exec = Button(text="Backup", command=backup)
-lbl_result = Label()
-lbl_intro = Label()
-lbl_intro["text"] = "Backup process does not alter your current save directory."
-lbl_logo = Label()
-lbl_logo["text"] = "Backup your saves, choom. It'll save your ass one day."
 
-lbl_logo.grid(row=3, column=0)
+lbl_result = Label(text="Ready!")
+lbl_intro = Label(text="Backup process does not alter your current save directory.")
+lbl_note = Label(text="Backup your saves, choom. It'll save your ass one day.")
+
+lbl_note.grid(row=3, column=0)
 lbl_intro.grid(row=1, column=1)
 btn_exec.grid(row=3, column=1)
 lbl_result.grid(row=4, column=1)
 
 window.mainloop()
-
