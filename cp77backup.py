980bc9893361934adaae14a32c746931b4d9e3e7
S. Hall <shall.usnavy@gmail.com>
2022-10-04 22:19:11 -0400

    feat: Add Tkinter GUI
    Brings a GUI alternative to the terminal/console version

--- cp77backup.py
+++ cp77backup.py
@@ -2,30 +2,23 @@ import shutil
 from datetime import date
 from pathlib import Path
 import os
+from tkinter import *
 
 
-def backup():
+def backup() -> str:
     save_path = Path.home().joinpath('Saved Games', 'CD Projekt Red')
     source = os.path.join(save_path, "Cyberpunk 2077")
     if not os.path.exists(source):
-        print("Cyberpunk saved games folder cannot be located. Exiting.")
-        return
+        return "Cyberpunk saved games folder cannot be located. Exiting."
 
     today = date.today()
     dst_format = "Cyberpunk 2077" + today.strftime("__%d_%b_%Y")
     destination = os.path.join(save_path, dst_format)
-
-    print(f"Source: {source}\nDestination: {destination}")  # Do we need to keep this?
-
     source_size = get_dir_size(source)
-
-    print("Compressing backup, please wait ...")
     target = shutil.make_archive(destination, "zip", source)
-
     target_size = get_file_size(target)
 
-    print(f"Save folder size: {source_size}, compressed backup size: {target_size}")
-    print("Done.")
+    return f"Save folder size: {source_size}, compressed backup size: {target_size}. Done!"
 
 
 def get_dir_size(target_dir) -> str:
@@ -40,4 +33,24 @@ def get_file_size(target_file) -> str:
     return f"{raw_size:.1f}M"
 
 
-backup()
+# backup()
+# Tk GUI
+window = Tk()
+window.title("Cyberpunk 2077 Savegame Backup")
+window.columnconfigure(1, minsize=450)
+window.rowconfigure(5, minsize=150)
+
+btn_exec = Button(text="Backup", command=backup)
+lbl_result = Label()
+lbl_intro = Label()
+lbl_intro["text"] = "Backup process does not alter your current save directory."
+lbl_logo = Label()
+lbl_logo["text"] = "Backup your saves, choom. It'll save your ass one day."
+
+lbl_logo.grid(row=3, column=0)
+lbl_intro.grid(row=1, column=1)
+btn_exec.grid(row=3, column=1)
+lbl_result.grid(row=4, column=1)
+
+window.mainloop()
+
